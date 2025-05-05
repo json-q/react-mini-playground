bd224dd4f5d1742eb1bf5bf257285d1a346e6f8e
JSQ <j996730508@163.com>
2025-05-05 21:52:25 +0800

    refactor: use vite

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-const ReactCompilerConfig = {};
-
-module.exports = {
-  plugins: [
-    ["babel-plugin-react-compiler", ReactCompilerConfig], // must run first!
-    "@babel/plugin-syntax-jsx",
-    ["@babel/plugin-syntax-typescript", { isTSX: true }],
-  ],
-};
