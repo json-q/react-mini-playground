75553871f298d09cfc40ca36d3833f0a447aa212
JSQ <j996730508@163.com>
2025-05-05 21:56:58 +0800

    feat: add build compress

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { fileURLToPath } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
+import { compression } from "vite-plugin-compression2";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), compression({ threshold: 10240 })],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
